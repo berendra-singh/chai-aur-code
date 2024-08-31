5e19146f4c3361b8fc06f20c815b5ea11bfae2c0
berendra-singh <beerendrasingh41456@gmail.com>
2024-08-31 09:12:34 +0000

    basic converstion java

---
+++ .devcontainer/converstion.js
@@ -0,0 +1,18 @@
+let age="69"
+console.log(typeof age )
+let value=Number(age)// converstion number me lene ke liye capital Number
+console.log(typeof value)
+// "33"=>33
+//"33abc"=>Nan
+//true=>1,false=>0
+//boolean converstion
+let isLogged=1
+let boolean=Boolean(isLogged)
+console.log(boolean)
+//boolean nme convert ke liye capital Boolean
+//""=>false
+//"anuj"=>true
+let num=33
+let str=String(num)
+console.log(str)
+console.log(typeof str)
